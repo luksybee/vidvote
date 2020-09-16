f1815c2ee979cf42294c298262151f2809abb9cd
Bolajoko Olayinka Luqman <50038106+luksyb@users.noreply.github.com>
2020-09-16 13:00:40 +0100

    WIP - vote feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'; // add this
 import { PostModule } from './post/post.module';
+import { VoteModule } from './vote/vote.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/vidvote', { useNewUrlParser: true }),
     PostModule,
+    VoteModule,
   ],
 })
 export class AppModule { }
